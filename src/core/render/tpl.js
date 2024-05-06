97abe28ec918dea032e69901c8ccef0dac399a42
baodinh1010 <nhobao.dinh@activate.sg>
2024-05-06 11:28:37 +0800

    fix breadcrumb url due to sidebar loading

--- src/core/render/tpl.js
+++ src/core/render/tpl.js
@@ -50,6 +50,18 @@ export function main(config) {
       : '') +
     '<div class="sidebar-nav"><!--sidebar--></div>' +
     '</aside>';
+
+  const hideSidebar = config.hideSidebar;
+  if (hideSidebar) {
+    return (
+      `<main>` +
+      '<section class="content">' +
+      '<section class="content-without-sidebar" style="padding-top: 122px">' +
+      '</section>' +
+      '</main>'
+    );
+  }
+
   return (
     `<main>${aside}` +
     '<section class="content">' +
